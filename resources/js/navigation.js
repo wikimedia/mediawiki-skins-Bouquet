48c127519c85a0bbf2597464d10a4f2e5297589c
Lewis Cawte <lewis@lewiscawte.me>
2014-11-10 16:45:21 +0000

    .jshintrc based on Nimbus skin.
    Change-Id: I3975e9d6878c362995948ab3622a37a22a1b4d5e

--- resources/js/navigation.js
+++ resources/js/navigation.js
@@ -3,21 +3,22 @@
  *
  * Handles toggling the navigation menu for small screens.
  */
+/* global document */
 ( function() {
 	var container = document.getElementById( 'access' );
 	var button    = container.getElementsByTagName( 'h1' )[0],
 		menu      = container.getElementsByTagName( 'ul' )[0];
 
-	if ( undefined == button || undefined == menu ) {
+	if ( undefined === button || undefined === menu ) {
 		return false;
 	}
 
 	button.onclick = function() {
-		if ( menu.className.indexOf( 'nav-menu' ) == -1 ) {
+		if ( menu.className.indexOf( 'nav-menu' ) === -1 ) {
 			menu.className = 'nav-menu';
 		}
 
-		if ( button.className.indexOf( 'toggled-on' ) != -1 ) {
+		if ( button.className.indexOf( 'toggled-on' ) !== -1 ) {
 			button.className = button.className.replace( ' toggled-on', '' );
 			menu.className = menu.className.replace( ' toggled-on', '' );
 			container.className = container.className.replace( 'main-small-navigation', 'navigation-main' );
