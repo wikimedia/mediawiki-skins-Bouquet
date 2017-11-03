152a090c0793092cc0ada82c82e047960b18492e
SamanthaNguyen <samanthanguyen1116@gmail.com>
2017-11-02 23:49:29 -0500

    Split classes into their own files and folder
    Classes are now their own file, as well as in the folder `/includes`.
    
    Change-Id: I980159f34bcd3d7f7fe0f5d2055ed3e06e6e4332

---
+++ includes/SkinBouquet.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Inherit main code from SkinTemplate, set the CSS and template filter.
+ *
+ * @ingroup Skins
+ */
+class SkinBouquet extends SkinTemplate {
+	public $skinname = 'bouquet', $stylename = 'bouquet',
+		$template = 'BouquetTemplate', $useHeadElement = true;
+
+	/**
+	 * Load the skin's JS via ResourceLoader.
+	 *
+	 * @param $out OutputPage
+	 */
+	public function initPage( OutputPage $out ) {
+		parent::initPage( $out );
+
+		// Add JS
+		$out->addModules( 'skins.bouquet.js' );
+	}
+
+	/**
+	 * @param $out OutputPage
+	 */
+	function setupSkinUserCss( OutputPage $out ) {
+		parent::setupSkinUserCss( $out );
+
+		// Load CSS via ResourceLoader
+		$out->addModuleStyles( array(
+			'mediawiki.skinning.interface',
+			'mediawiki.skinning.content.externallinks',
+			'skins.bouquet'
+		) );
+	}
+}
