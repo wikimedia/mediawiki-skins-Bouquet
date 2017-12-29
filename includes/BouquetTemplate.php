870cea63fed745338d69b737e1d0f4bddf85b268
LukBukkit <luk.bukkit@gmail.com>
2017-12-29 22:50:28 +0100

    Avoid PHP 7.1 warning of &$this usage
    Bug: T153505
    Change-Id: Ic21748bacc4c7281f0b85d0bd6accb285f2de087

--- includes/BouquetTemplate.php
+++ includes/BouquetTemplate.php
@@ -317,7 +317,9 @@ class BouquetTemplate extends BaseTemplate {
 			echo $this->makeListItem( $key, $tbItem );
 		}
 
-		Hooks::run( 'SkinTemplateToolboxEnd', array( &$this, true ) );
+		// Avoid PHP 7.1 warning of passing $this by reference
+		$template = $this;
+		Hooks::run( 'SkinTemplateToolboxEnd', array( &$template, true ) );
 ?>
 		</ul>
 	</aside>
